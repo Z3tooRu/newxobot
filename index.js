0f8c622be5ead6dd0dcec17c7040934441e6e3bb
Z3tooRu <z3tooory@gmail.com>
2024-10-29 01:51:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const express = require("express")
-const app = express();
-var listener = app.listen(process.env.PORT || 2000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
-app.listen(() => console.log("I'm Ready To Work..! 24H"));
-app.get('/', (req, res) => {
-  res.send(`
-  <body>
-  <center><h1>Bot 24H ON!</h1></center
-  </body>`)
-});
-
 const { GatewayIntentBits, Integration, ChannelFlags, AttachmentBuilder } = require('discord.js');
 const Discord = require('discord.js');
 const {
